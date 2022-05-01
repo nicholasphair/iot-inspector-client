ce26f643396c991231c04e05634b230093765f5d
Nick Phair <nickphair@gmail.com>
2022-04-30 23:22:07 -0400

    write flow and dns data to db

--- src/inspector/dbdumper.py
+++ src/inspector/dbdumper.py
@@ -61,11 +61,77 @@ class DBDumper(object):
         ddata = utils.deserialize_data(data)
         self._write_to_db(ddata)
 
+    def _flow_to_record(self, flow):
+        k, value = flow
+        key = json.loads(k)
+
+        device_id = f"'{key[0]}'"
+        device_port = str(key[1])
+        remote_ip = f"'{key[2]}'"
+        remote_port = str(key[3])
+        protocol = f"'{key[4]}'"
+        in_byte_count = value['inbound_byte_count']
+        out_byte_count = value['outbound_byte_count']
+
+        # NB (nphair): Not sure about these fields.
+        is_inspected = '0'
+        remote_hostname = remote_ip
+        remote_hostname_info_source = "''"
+        remote_ip_country = "''"
+        remote_reg_domain = "''"
+        remote_tracker = "''"
+        remote_web_xray = "''"
+        total_byte_count = in_byte_count + out_byte_count
+        ts = int(value['internal_flow_ts_min'])
+        ts_min = value['internal_flow_ts_min']
+        ts_mod10 = ts % 10
+        ts_mod3600 = ts % 3600
+        ts_mod60 = ts % 60
+        ts_mod600 = ts % 600
+        user_key = f'test_{str(uuid.uuid4()).replace("-", "")}'
+        user_key = f"'{user_key}'"
+        return ', '.join((
+            device_id,
+            device_port,
+            str(in_byte_count),
+            is_inspected,
+            str(out_byte_count),
+            protocol,
+            remote_hostname,
+            remote_hostname_info_source,
+            remote_ip,
+            remote_ip_country,
+            remote_port,
+            remote_reg_domain,
+            remote_tracker,
+            remote_web_xray,
+            str(total_byte_count),
+            str(ts),
+            str(ts_min),
+            str(ts_mod10),
+            str(ts_mod3600),
+            str(ts_mod60),
+            str(ts_mod600),
+            user_key,
+        ))
+
+    def _dns_to_records(self, dns):
+        k, value = dns
+        key = json.loads(k)
+
+        user_key = f'test_{str(uuid.uuid4()).replace("-", "")}'
+        user_key = f"'{user_key}'"
+        device_id = f"'{key[0]}'"
+        ts = "0"
+        hostname = f"'{key[1]}'"
+        ip = f"'{key[2]}'"
+        device_port = str(key[3])
+        data = ', '.join((user_key, device_id, ts, ip, hostname, device_port))
+        return [f"{data}, '{ds}'" for ds in value]
+
     def _write_to_db(self, data):
-        #         device_id and device_name in the devices table
-        # device_id, ts, and hostname in the dns table
-        # device_id, device_port, in_byte_count, out_byte_count, remote_port, and ts from flows
-        if data: print(data)
+        if data:
+            print(data)
         client_status_text = data['client_status_text']
         dns_dict = data['dns_dict']
         syn_scan_dict = data['syn_scan_dict']
@@ -80,17 +146,14 @@ class DBDumper(object):
         duration = data['duration']
         client_ts = data['client_ts']
 
-        for device, items in netdisco_dict.items():
+        for device_id, items in netdisco_dict.items():
             for ndd in items:
-                device_id = ndd.get('serial')
                 dhcp_hostname = ndd.get('host')
                 device_ip = ndd.get('host')
                 device_name = ndd.get('model_name')
                 device_type = ndd.get('upnp_device_type')
                 device_vendor = ndd.get('manufacturer')
-                device_oui = device
-                netdisco_name = ndd.get('name')
-                fb_name = 'baz'
+                device_oui = device_id
                 vals = ', '.join((
                     f"'{device_id}'",
                     f"'{dhcp_hostname}'",
@@ -106,6 +169,16 @@ class DBDumper(object):
                     f"0",    # TODO
                 ))
                 self._conn.execute(f"INSERT INTO devices VALUES ({vals})")
+
+        for dns in dns_dict.items():
+            for record in self._dns_to_records(dns):
+                print(record)
+                self._conn.execute(f"INSERT INTO dns VALUES ({record})")
+
+        for flow in flow_dict.items():
+            record = self._flow_to_record(flow)
+            self._conn.execute(f"INSERT INTO flows VALUES ({record})")
+
         self._conn.commit()
 
     def _table_exists(self, table_name):
