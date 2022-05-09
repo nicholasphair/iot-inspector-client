46cba6ab4ee87c156fb60a8d9b4634a24f0ad169
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2022-05-09 01:55:04 -0600

    Add an argument to run_demo.py that copies in a data file

--- run_demo.py
+++ run_demo.py
@@ -2,6 +2,7 @@
 
 import argparse
 import logging
+import shutil
 from pathlib import Path
 
 home_dir = Path.home() / "princeton-iot-inspector" / ".configs"
@@ -23,6 +24,10 @@ if __name__ == "__main__":
         type=int,
     )
     parser.add_argument(
+        "data_file",
+        help="Parquet file with data for client",
+    )
+    parser.add_argument(
         "-p",
         "--peers",
         nargs="+",
@@ -35,10 +40,16 @@ if __name__ == "__main__":
 
     # Create a file to designate the client with PID updater_pid
     # to initiate a computation
-    (home_dir / str(args.updater_pid) / "start_computation").touch()
+    initiator_dir = home_dir / str(args.updater_pid)
+    (initiator_dir / "start_computation").touch()
     logger.info("Designated PID %d as initiator", args.updater_pid)
 
     peers = args.peers if args.peers is not None else []
     for peer in peers:
         (home_dir / str(peer) / "start_peer").touch()
         logger.info("Designated PID %d as a peer", peer)
+
+    data_file = Path(args.data_file)
+    output_path = initiator_dir / "features.pq"
+    shutil.copy(data_file, initiator_dir / "features.pq")
+    logger.info("Wrote %s to %s", data_file, output_path)
